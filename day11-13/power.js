4ade7a86a499c8630064a2a075fd3a63020b73da
muhammad <bryanedwarding@email.com>
2025-04-27 18:47:07 +0100

    feat(day11-13): added level up and reset feautures

--- day11-13/power.js
+++ day11-13/power.js
@@ -1,6 +1,7 @@
 let header = document.querySelector("h1");
 let header4 = document.querySelector("h4");
-let lvlBtn = document.querySelector("button");
+let lvlBtn = document.querySelector("#level");
+let resetBtn = document.querySelector("#btn");
 let img = document.querySelector("img");
 
 const character = {
@@ -14,28 +15,55 @@ const character = {
       "Yokoso, I am " + this.name + " and I am " + this.age + " years old!";
     header4.textContent = "I am from " + this.origin + " and " + this.power;
   },
+
   levelUp: function () {
-    this.age += 100;
-    header.textContent =
-      "Yokoso, I am " +
-      this.name +
-      " and I am " +
-      this.age +
-      " years old! I have leveled up!";
-    header4.textContent =
-      "I am from " +
-      this.origin +
-      " and " +
-      this.power +
-      ". I have leveled up!";
-    lvlBtn.style.backgroundColor = "green";
-    lvlBtn.style.color = "white";
-    img.src = `assets/lvl2.jpeg`;
+    if (this.age == 1000) {
+      this.age += 100;
+      this.power = "I have Immortality";
+      img.src = `assets/lvl2.jpeg`;
+    } else if (this.age == 1100) {
+      this.age += 200;
+      this.power = "I have Kyoka Suigetsu Illusion";
+      img.src = `assets/lvl3.jpeg`;
+    } else if (this.age == 1300) {
+      this.age += 300;
+      this.power = "I created Reiatsu Crushing";
+      img.src = `assets/lvl4.jpeg`;
+    } else if (this.age == 1600) {
+      this.age += 400;
+      this.power = "I'm the strongest";
+      img.src = `assets/lvl5.jpeg`;
+
+      // 🧠 Hide "Level Up" button and show "Base Form" button
+      lvlBtn.style.display = "none";
+      resetBtn.style.display = "block";
+    }
+
+    header.textContent = `Yokoso, I am ${this.name} and I am ${this.age} years old! I have leveled up!`;
+    header4.textContent = `I am from ${this.origin} and ${this.power}`;
+  },
+
+  resetForm: function () {
+    this.age = 1000;
+    this.power = "I have the power to alter reality";
+    img.src = `assets/lvl1.jpeg`;
+
+    // 🧠 Show "Level Up" button again and hide "Base Form" button
+    lvlBtn.style.display = "block";
+    resetBtn.style.display = "none";
+
+    this.intro();
   },
 };
+
+// 👆 Button click events
 lvlBtn.addEventListener("click", function () {
   character.levelUp();
 });
 
+resetBtn.addEventListener("click", function () {
+  character.resetForm();
+});
 
+// 👏 Load first intro
 character.intro();
